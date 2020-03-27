dee5176c67414c26bfa6993b5904d15de91dccae
HARADA Takashi <harada.takashi@kochi-tech.ac.jp>
2020-03-27 15:01:12 +0900

    adapt classbench

--- harada_main.c
+++ harada_main.c
@@ -5,13 +5,25 @@
 #include "include/DdList.h"
 #include "include/MDD.h"
 #include "include/rule.h"
+#include "include/header.h"
 #include "include/rbt.h"
 #include <sys/time.h>
 #include <time.h>
 
+void write_dd(DdManager* gm, DdNode *dd, char* filename) {
+    FILE *outfile; // output file pointer for .dot file
+    outfile = fopen(filename,"w");
+    DdNode **ddnodearray = (DdNode**)malloc(sizeof(DdNode*)); // initialize the function array
+    ddnodearray[0] = dd;
+    Cudd_DumpDot(gm, 1, ddnodearray, NULL, NULL, outfile); // dump the function to .dot file
+    free(ddnodearray);
+    fclose (outfile); // close the file */
+}
+
+
 DdNode* makeDdNode(DdManager *gbm, char *rule){
   
-  DdNode *bdd,*var,*tmp;
+  DdNode *bdd, *var, *tmp;
   int i;
 
   bdd = Cudd_ReadOne(gbm); /*Returns the logic one constant of the manager*/
@@ -43,35 +55,38 @@ int main(int argc, char *argv[]) /* ./MyMain RuleListFile NumberOfRules BitNumbe
 {
   //--------------------------------------------------------------- make MTBDD from RuleList
 
+
+  /****** 引数の数とルールリストファイルのチェック開始 *****/
   if (4 != argc) {
     fprintf(stderr, "ERROR!! The number of arguments is invalid\n");
     exit(1);
   }
-  
   FILE *fp;
   if((fp = fopen(argv[1],"r")) == NULL){
     printf("RuleList file open error!!\n");
     exit(EXIT_FAILURE);
   }
+  /****** 引数の数とルールリストファイルのチェック終了 *****/
 
+  /****** ルール数 (ruleSize) のカウント開始 *****/
   char dummy[255], cheader[255] /*, filename[30]="mmresult.dot" */;
   /* int ruleSize = atoi(argv[2]); */
   int ruleSize = 0;
-  for( ; fgets(dummy, 256, fp) != NULL; ++ruleSize)
-    ;
+  for( ; fgets(dummy, 256, fp) != NULL; ++ruleSize) { ; }
+  /****** ルール数 (ruleSize) のカウント終了 *****/
 
+  /****** ヘッダファイルのチェックとヘッダ数 (headerSize) のカウント開始 *****/
   FILE *fp2;
   if((fp2 = fopen(argv[3],"r")) == NULL){
     printf("HeaderList file open error!!\n");
     exit(EXIT_FAILURE);
   }
-
   int headerSize = 0;
-  for( ;fgets(cheader,256,fp2) != NULL; ++headerSize)
-    ;
+  for( ;fgets(cheader,256,fp2) != NULL; ++headerSize) { ; }
+  /****** ヘッダファイルのチェックとヘッダ数 (headerSize) のカウント終了 *****/
+  /* printf("ruleSize = %d, atoi(argv[2]) = %d, headerSize = %d\n", ruleSize, atoi(argv[2]), headerSize); */
 
-  printf("ruleSize = %d, atoi(argv[2]) = %d, headerSize = %d\n", ruleSize, atoi(argv[2]), headerSize);
-
+  /****** MTBDD作成準備 ******/
   DdManager *gbm;
   DdNode *bdd, *mtbdd, *submtbdd, *tmp, *tmp2;
   DdList *terminalList, *terminalIndex, *fwdIndex; // for MDD
@@ -88,6 +103,9 @@ int main(int argc, char *argv[]) /* ./MyMain RuleListFile NumberOfRules BitNumbe
 
   rewind(fp);
   int i;
+  struct timeval start, timeofmdd, clatimeofmdd;
+  gettimeofday(&start,NULL);
+
   for(i = 1; i <= ruleSize; i++) {
     int SA1, SA2, SA3, SA4, SAPrefix, DA1, DA2, DA3, DA4, DAPrefix;
     int SPL, SPR, DPL, DPR, PROT, PROTMask;
@@ -97,41 +115,45 @@ int main(int argc, char *argv[]) /* ./MyMain RuleListFile NumberOfRules BitNumbe
     /* rule_print(r); putchar('\n'); */
     char zms[105]; zms[104] = '\0';
     unsigned j;
-    /* SA and DA */
+    /**** SA and DA 送信元アドレス(32bit)と宛先アドレス(32bit)分の0,1,*の文字列を zms にコピー ****/
     for (j = 0; j < 32; ++j)
       zms[j] = r->sa[j], zms[j+32] = r->da[j];
-    /* PROT */
+    /**** PROT プロトコル(8bit)分の0,1,*の文字列を zms にコピー ****/
     for (j = 0; j < 8; ++j)
       zms[PROT_START_POINT+j] = r->prot[j];
-    /* SP and DP */
+    /**** SP and DP 送信元ポート番号(16bit)と宛先ポート番号(16bit)分の 0,1,*の文字列をコピー *****/
+    /*** ポート番号は x : y の形であり，一つの 0,1,* の文字列では表現できないことがあることに注意 ***/
+    /** 送信元ポート番号を 0,1,* の文字列のリストへ (ls) と変換 **/
     list_string* ls = range_to_01ms(r->spl, r->spr, DEFAULTPORTNUMBER_L, DEFAULTPORTNUMBER_R);
+    /** 送信元ポート番号を 0,1,* の文字列のリストへ (ld) と変換 **/
     list_string* ld = range_to_01ms(r->dpl, r->dpr, DEFAULTPORTNUMBER_L, DEFAULTPORTNUMBER_R);
     list_string_cell* ptr_s;
+    /* 送信元ポート番号の文字列と宛先ポート番号の文字列それぞれでループを回す */
     for (ptr_s = ls->head; NULL != ptr_s; ptr_s = ptr_s->next) {
       for (j = 0; j < 16; ++j)
-	zms[SP_START_POINT+j] = ptr_s->key[j];
+	zms[SP_START_POINT+j] = ptr_s->key[j]; 
       list_string_cell* ptr_d;
       for (ptr_d = ld->head; NULL != ptr_d; ptr_d = ptr_d->next) {
 	for (j = 0; j < 16; ++j)
 	  zms[DP_START_POINT+j] = ptr_d->key[j];
 	/* printf("zms = %s\n", zms); */
-	terminalIndex->node = Cudd_addConst(gbm, i);
+	terminalIndex->node = Cudd_addConst(gbm, i); /* 終端節点 i を追加 */
 	Cudd_Ref(terminalIndex->node);
-	bdd = makeDdNode(gbm, zms);
+	bdd = makeDdNode(gbm, zms); /* ルール zms に対する BDD を構築 */
 	tmp = Cudd_BddToAdd(gbm, bdd);
 	Cudd_Ref(tmp);
 	Cudd_RecursiveDeref(gbm, bdd);
 	bdd = tmp;
-	submtbdd = Cudd_addApply(gbm, Cudd_addTimes, terminalIndex->node, bdd);
+	submtbdd = Cudd_addApply(gbm, Cudd_addTimes, terminalIndex->node, bdd); /* MTBDD へと変換 */
 	Cudd_Ref(submtbdd);
 	Cudd_RecursiveDeref(gbm, bdd);
       
-	tmp2 = Cudd_addApply(gbm, Cudd_addUni, mtbdd, submtbdd);
+	tmp2 = Cudd_addApply(gbm, Cudd_addUni, mtbdd, submtbdd); /* 既存の MTBDD とマージ */
 	Cudd_Ref(tmp2);
 	Cudd_RecursiveDeref(gbm,mtbdd);
 	Cudd_RecursiveDeref(gbm,submtbdd);
 	mtbdd = tmp2;
-	if((terminalIndex -> next = (DdList*)malloc(sizeof(DdList))) == NULL){ //for MDD
+	if((terminalIndex->next = (DdList*)malloc(sizeof(DdList))) == NULL){ //for MDD
 	  fprintf(stderr, "memory for terminalIndex error!!\n");
 	  exit(EXIT_FAILURE);
 	}
@@ -143,8 +165,9 @@ int main(int argc, char *argv[]) /* ./MyMain RuleListFile NumberOfRules BitNumbe
     list_string_clear(ls); ls = NULL;
     rule_clear(r); r = NULL;
   }
-  // for default rule
-  // FIXME
+
+  /***** デフォルトルールの作成開始
+	 （入力ルールリストファイルにデフォルトルールは含まれないことに注意 *****/
   const int bitlen = 104;
   char def[bitlen+1];
   for(i = 0; i < bitlen; i++)
@@ -173,75 +196,82 @@ int main(int argc, char *argv[]) /* ./MyMain RuleListFile NumberOfRules BitNumbe
   }
   fwdIndex = terminalIndex; // for MDD
   terminalIndex = terminalIndex -> next; // for MDD
+
+  /***** デフォルトルール作成終了 *****/
   
   free(terminalIndex);
   fwdIndex->next = NULL;
   fclose(fp);
 
-  /* int *headerList[headerSize], *header, k = atoi(argv[2]); */
-  /* rewind(fp2); */
-  /* int sum, h, j; */
-  /* for(i=0;fgets(cheader,256,fp2) != NULL;i++){ */
-  /*   int pkt[bitlen]; */
-  /*   if(( header = (int*)malloc(sizeof(int)*((int)(bitlen/k+0.9)))) == NULL){ */
-  /*     printf("memory for header error!!\n"); */
-  /*     exit(EXIT_FAILURE); */
-  /*   } */
-  /*   sum = 0; */
-  /*   h = k-1; */
-  /*   for(j=0;j<bitlen;j++){ */
-  /*     pkt[j] = cheader[j] - '0'; */
-  /*     sum += pkt[j]*(1<<h); */
-  /*     if(h == 0){ */
-  /* 	header[j/k] = sum; */
-  /* 	sum = 0; */
-  /* 	h = k-1; */
-  /*     } */
-  /*     else */
-  /* 	h--; */
-  /*   } */
-  /*   if(h != k-1) */
-  /*     header[(j-1)/k] = sum; */
-  /*   //printf("%d\n",(int)Cudd_V(Cudd_Eval(gbm,mtbdd,pkt))); */
-  /*   if( Cudd_IsConstant(Cudd_Eval(gbm,mtbdd,pkt)) != 1){ */
-  /*     printf("mtbdd classfication error!! \n"); */
-  /*     exit(EXIT_FAILURE); */
-  /*   } */
-  /*   headerList[i] = header; */
-  /* } */
-  /* fclose(fp2); */
+  /* write_dd(gbm, mtbdd, "mtbdd.dot"); */
 
-  /* //------------------------------------------------------------------- make MDD from MTBDD */
-  /* struct timeval start,timeofmtbdd,timeofmdd,clatimeofmtbdd,clatimeofmdd; */
-  /* gettimeofday(&start,NULL); */
-  
-  /* unsigned int hashSize = Cudd_DagSize(mtbdd)*10; */
-  /* keepMemoryForHash(hashSize,terminalList,k); */
-  /* free_behaviors(terminalList); */
-  /* MddNode *mdd; */
-  /* mdd = makeMDD(k,gbm,mtbdd); */
+  /* ヘッダリストの読み込み */
+  int k = atoi(argv[2]);
+  int *headerList[headerSize], *iheader;
+  rewind(fp2);
+  int sum, h, j;
+  long SA, DA, SP, DP, PROT, FLAG;
+  for (i = 0; EOF != fscanf(fp2, "%ld %ld %ld %ld %ld %ld", &SA, &DA, &SP, &DP, &PROT, &FLAG); ++i) {
+    header* hh = mk_header(i, SA, DA, SP, DP, PROT);
+    int pkt[bitlen];
+    if(NULL == (iheader = (int*)malloc(sizeof(int)*((int)(bitlen/k+0.9))))) {
+      printf("memory for header error!!\n");
+      exit(EXIT_FAILURE);
+    }
+    sum = 0;
+    h = k-1;
+    for(j = 0; j < bitlen; j++) {
+      // pkt[j] = cheader[j] - '0';
+      pkt[j] = hh->string[j] - '0';
+      sum += pkt[j]*(1<<h);
+      if(h == 0){
+  	iheader[j/k] = sum;
+  	sum = 0;
+  	h = k-1;
+      }
+      else
+  	h--;
+    }
+    if(h != k-1)
+      iheader[(j-1)/k] = sum;
+    //printf("%d\n",(int)Cudd_V(Cudd_Eval(gbm,mtbdd,pkt)));
+    if( Cudd_IsConstant(Cudd_Eval(gbm,mtbdd,pkt)) != 1){
+      printf("mtbdd classfication error!! \n");
+      exit(EXIT_FAILURE);
+    }
+    headerList[i] = iheader;
+    header_clear(hh); hh = NULL;
+  }
+  fclose(fp2);
 
-  /* gettimeofday(&timeofmdd,NULL); */
-  /* printf("time of making MDD = %lf s\n",(timeofmdd.tv_sec - start.tv_sec) + (timeofmdd.tv_usec - start.tv_usec)*1.0e-6 ); */
+  /* ------------------------------------------------------------------- make MDD from MTBDD */  
+  unsigned int hashSize = Cudd_DagSize(mtbdd)*10;
+  keepMemoryForHash(hashSize, terminalList, k);
+  free_behaviors(terminalList);
+  MddNode *mdd;
+  mdd = makeMDD(k, gbm, mtbdd);
 
-  /* struct timespec s, e; */
-  /* clock_gettime(CLOCK_REALTIME, &s); */
-  /* for(i=0;i<headerSize;i++){ */
-  /*   //printf("[%d,%d,%d]\n",*(headerList[i]),*(headerList[i]+1),*(headerList[i]+2)); */
-  /*   //printf("%d\n",MDD_Eval(bitlen,k,mdd,headerList[i])->value); */
-  /*   if( MDD_Eval(bitlen,k,mdd,headerList[i])->value == -1){ */
-  /*     printf("mdd classfication error!! \n"); */
-  /*     exit(EXIT_FAILURE); */
-  /*   } */
-  /* } */
-  /* clock_gettime(CLOCK_REALTIME, &e); */
-  /* gettimeofday(&clatimeofmdd,NULL); */
+  gettimeofday(&timeofmdd,NULL);
+  printf("time of making MDD = %lf s\n",(timeofmdd.tv_sec - start.tv_sec) + (timeofmdd.tv_usec - start.tv_usec)*1.0e-6 );
 
-  /* if (e.tv_nsec < s.tv_nsec) { */
-  /*   printf("MDD Search Time = %10ld.%09ld\n", e.tv_sec - s.tv_sec - 1, e.tv_nsec + 1000000000 - s.tv_nsec); */
-  /* } else { */
-  /*   printf("MDD Search Time = %10ld.%09ld\n", e.tv_sec - s.tv_sec, e.tv_nsec - s.tv_nsec); */
-  /* }     */
+  struct timespec s, e;
+  clock_gettime(CLOCK_REALTIME, &s);
+  for (i = 0; i < headerSize; i++) {
+    //printf("[%d,%d,%d]\n",*(headerList[i]),*(headerList[i]+1),*(headerList[i]+2));
+    //printf("%d\n",MDD_Eval(bitlen,k,mdd,headerList[i])->value);
+    if (-1 == MDD_Eval(bitlen,k,mdd,headerList[i])->value) {
+      printf("mdd classfication error!! \n");
+      exit(EXIT_FAILURE);
+    }
+  }
+  clock_gettime(CLOCK_REALTIME, &e);
+  gettimeofday(&clatimeofmdd, NULL);
+
+  if (e.tv_nsec < s.tv_nsec) {
+    printf("MDD Search Time = %10ld.%09ld\n", e.tv_sec - s.tv_sec - 1, e.tv_nsec + 1000000000 - s.tv_nsec);
+  } else {
+    printf("MDD Search Time = %10ld.%09ld\n", e.tv_sec - s.tv_sec, e.tv_nsec - s.tv_nsec);
+  }
   
   /* /\* printf("classfication time of MDD = %lf ms\n",(clatimeofmdd.tv_sec - timeofmdd.tv_sec)*1000 + (clatimeofmdd.tv_usec - timeofmdd.tv_usec)*1.0e-3 ); *\/ */
   
